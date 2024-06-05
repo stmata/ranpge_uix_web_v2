f8fc2dc43978ebe2daecd186c855aac239a400d5
stmata <108939392+stmata@users.noreply.github.com>
2024-06-05 14:58:46 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  moduleNameMapper: {
+    'next/router': '<rootDir>/__mocks__/next/router.js',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/__mocks__/file-mock.js',
+    'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
+  },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
