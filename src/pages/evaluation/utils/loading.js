c1e5c24e3b11c02d62f4121a2743eb9bf46d3434
stmata <108939392+stmata@users.noreply.github.com>
2024-06-05 17:03:52 -0400

    Update loading.js
    no-unused-vars

--- src/pages/evaluation/utils/loading.js
+++ src/pages/evaluation/utils/loading.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import LottieAnimation from './animation';
 import './evaluation.css'
 import { useStateGlobal } from '../../../context/contextStateGlobale';
