9b5b9ff09eab0fd6f7537e5d2510d7941e5484ca
stmata <108939392+stmata@users.noreply.github.com>
2024-06-05 15:37:29 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  moduleNameMapper: {
-    'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
-  },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
